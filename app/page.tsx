306963a2393f46ba4932856919474e3b4f6d46bd
Sybatron <vipsister2014@gmail.com>
2026-04-27 19:22:55 +0300

    add account settings card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
+import { AccountSettingsForm } from "@/components/account-settings-form";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Field } from "@/components/ui/field";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<Card className="mx-8 my-16 w-full max-w-4xl gap-8">
+				<CardHeader>
+					<CardTitle className="text-xl font-semibold">
+						Manage Your Account
+					</CardTitle>
+					<CardDescription>
+						Update your account details below for a tailored
+						experience on our platform.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<AccountSettingsForm />
+				</CardContent>
+			</Card>
+		</main>
+	);
 }
